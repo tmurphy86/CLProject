3e28696311c5dbb0821b5f7d5d8ddf44b8557ae1
Saveliy <samoylov3@hotmail.com>
2017-10-08 16:46:53 -0400

    create first button component

---
+++ client/src/components/Buttons/PrimaryButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Buttons.css';
+
+const PrimaryButton = (props) => {
+  return (
+    <a href={props.href} className='clButton PrimaryButton bold-text'>{props.children}</a>
+  )
+}
+
+export default PrimaryButton;
