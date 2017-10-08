a0e4799891967d4f85cf2b87ccd9c799369f8d0b
Saveliy <samoylov3@hotmail.com>
2017-10-08 19:25:58 -0400

    created search components

---
+++ client/src/components/Search/NewLocation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const NewLocation = (props) => {
+  return (
+    <div className="newLocation"><i class="fa fa-globe" aria-hidden="true"></i>{props.location}</div>
+  )
+}
+
+export default NewLocation;
--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import NewLocation from './NewLocation.js'
+import "./Search.css";
 
 class Search extends React.Component {
   render(){
     return (
-      <h1>Search Component</h1>
+      <form className="Search-form">
+        <input type="text" for="search" placeholder="Search"/>
+        <i class="fa fa-search searchIcon" aria-hidden="true"></i>
+        <span>in</span>
+        <NewLocation location='Arlington, Virginia'/>
+      </form>
     )
   }
 }
