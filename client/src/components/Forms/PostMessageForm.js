0d509e37aff0912a5a52365bd8f2c3c21eb1cd96
Saveliy <samoylov3@hotmail.com>
2017-10-15 21:20:05 -0400

    got the post message form to 'submit' ... just console.logging for now.

--- client/src/components/Forms/PostMessageForm.js
+++ client/src/components/Forms/PostMessageForm.js
@@ -2,10 +2,42 @@ import React from 'react';
 import "./Forms.css";
 
 class PostMessageForm extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      messageVal: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      messageVal: e.target.value
+    })
+  }
+
+  submitMessage = (e) => {
+    e.preventDefault();
+
+    if(this.state.messageVal){
+      // Submit form.
+      console.log(this.state.messageVal)
+    } else {
+      // Throw error. Don't submit form.
+      console.log("empty")
+    }
+
+
+
+  }
+
+
+
   render(){
     return (
         <form className="post-message-form">
-            <textarea rows="1" placeholder="Type your private message"/>
+            <textarea rows="3" placeholder="Type your private message" value={this.state.messageVal} onChange={this.handleChange}/>
+            <button className="btn btn-primary btn-md pull-right post-message-form-submit" onClick={this.submitMessage}>SEND</button>
         </form>
     )
   }
